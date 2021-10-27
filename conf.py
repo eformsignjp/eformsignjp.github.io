4657074d45497476cfc8e276b83538e70b111aaa
SeoyoungChoii <sychoi@forcs.com>
2021-10-27 14:10:58 +0900

    테마 변경

--- conf.py
+++ conf.py
@@ -74,7 +74,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'press'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -86,6 +86,8 @@ html_theme = 'press'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_favicon = 'C:\docbook\eformsignjp.github.io\images\\favicon.ico'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -176,4 +178,4 @@ html_theme_options = {
     'logo_only': True,
     # ...
 }
-html_logo = "C:\docbook\eformsignjp.github.io\images\\forcs.png"
+html_logo = "C:\docbook\eformsignjp.github.io\images\\forcs_b.png"
