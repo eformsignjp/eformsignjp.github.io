584a2daf02513181e17bb83d2d2f96c54b4c5639
suzuki <suzuki@forcs.com>
2024-02-08 09:57:10 +0900

    * 2024년11월 업데이트 반영, 재업로드

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.04.07',
+    VERSION: '2024.01.31',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
